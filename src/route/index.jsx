abcb81d16473c45d8668b0ee49805a623dcdc4a5
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-24 00:42:53 +0100

    Added more route

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
+import Redirect from "../components/Redirect";
 import Contact from "../pages/Contact";
 import Home from "../pages/Home";
-import More from "../pages/More";
-import Random from "../pages/Random";
+import Female from "../pages/Female";
+import Male from "../pages/Male";
 import User from "../pages/User";
 import Users from "../pages/Users";
 
@@ -12,11 +13,12 @@ function ProjectRoute() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="users" element={<Users />}>
-          <Route path="more" element={<More />} />
+          <Route path="female" element={<Female />} />
+          <Route path="male" element={<Male />} />
         </Route>
-        <Route path="/user:id" element={<User />} />
-        <Route path="random" element={<Random />} />
+        <Route path="/user/:id" element={<User />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Redirect />} />
       </Routes>
     </>
   );
