696727a4032f94a68a25454cfe9c44bcd140277d
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-24 00:48:17 +0100

    A single user component

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,108 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { NavBar } from "../components";
+import BrowserMap from "../components/BrowserMap";
 
+import "./user.css";
 export default function User() {
+  const location = useLocation();
+  const { page, user } = location.state;
+  const navi = useNavigate();
+
   return (
-    <div>
+    <>
       <NavBar />
-      User
-    </div>
+      <div className="user-container">
+        {user && (
+          <div key={user.login.uuid} className="wrap">
+            <div>
+              <div className="details-sec">
+                <img
+                  src={user.picture.medium}
+                  alt={`${user.name.first}${user.name.last}`}
+                  className="cardimg"
+                />
+                <div>
+                  <p className="">
+                    <span>{user.name.first} </span>
+                    <span> {user.name.last} </span>
+                  </p>
+                  <div className="flex">
+                    {" "}
+                    <img
+                      src={"../assets/icons8-circled-envelope.svg"}
+                      alt="email icon"
+                      className="img-icon"
+                    />{" "}
+                    <span>{user.email}</span>
+                  </div>
+                  <div className="flex">
+                    {" "}
+                    <img
+                      src={"../assets/icons8-phone.svg"}
+                      alt="zip code icon"
+                      className="img-icon"
+                    />{" "}
+                    <span>{user.phone}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="full-details">
+                <p>{user.dob.age} years old</p>
+                <div className="flex">
+                  {" "}
+                  <img
+                    src={"../assets/icons8-gender.svg"}
+                    alt="gender icon"
+                    className="img-icon"
+                  />
+                  <span>{user.gender}</span>
+                </div>
+
+                <div className="flex">
+                  {" "}
+                  <img
+                    src={"../assets/icons8-user.svg"}
+                    alt="user icon"
+                    className="img-icon"
+                  />{" "}
+                  @{user.login.username}
+                </div>
+
+                <div className="flex">
+                  {" "}
+                  <img
+                    src={"../assets/icons8-zip-code.svg"}
+                    alt="zip code icon"
+                    className="img-icon"
+                  />{" "}
+                  {user.location.postcode}
+                </div>
+                <p className="flex">
+                  {" "}
+                  <img
+                    src={"../assets/icons8-city-buildings.svg"}
+                    alt="city icon"
+                    className="img-icon"
+                  />{" "}
+                  <span>{user.location.city},</span>
+                  <span>{user.location.country} </span>
+                </p>
+              </div>
+            </div>
+
+            <BrowserMap location={user.location} />
+            <div className="back">
+              <button
+                onClick={() => navi(-1)}
+                aria-label="go back"
+                className="btn arrow"
+              ></button>
+              <span className="text">go back</span>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
