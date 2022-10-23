e200d5a7c4936f71d212ab72912ef2e80b3d0c30
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-24 00:44:39 +0100

    Wrapped application with error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ErrorBoundary } from 'react-error-boundary'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import ErrorComponent from './components/ErrorComponent'
 import './index.css'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={ErrorComponent}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
