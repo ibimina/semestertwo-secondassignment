b2c91d0d03146a7efff4d8c9a22ee8dc384858a1
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-24 00:52:30 +0100

    home componenet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import{NavBar}from "../components"
 export default function Home() {
   return (
-    <div>
-      <NavBar/>
-    </div>
-  )
+    <>
+      <NavBar />
+      <div className="developer">
+        <h1>Home</h1>
+        <p>List of users</p>
+        <p>content developed with </p>
+        <p>Ibimina Hart</p>
+        
+      </div>
+    </>
+  );
 }
