99ece5af4d70b9b73d41b2f189927c1075e6f240
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-26 12:36:12 +0100

    Added filterGen function

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,38 +4,46 @@ import { useErrorHandler } from "react-error-boundary";
 export default function useFetch(url) {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
+
   const errorFun = useErrorHandler();
- const filterNat=(nat)=>{
-   setUsers((prev) => prev.filter((user) => nat.includes(user.nat)));
- }
- const filterGen = (gen) => {
-   setUsers((prev) => prev.filter((user) => gen.includes(user.gen)));
- };
+
   useEffect(() => {
-   const abortConst = new AbortController();
-   const fetchData = async () => {
-     try {
-       setLoading(true);
-       const res = await fetch(url, { signal: abortConst.signal });
-       const data = await res.json();
-       setLoading(false);
-       setUsers(data.results);
-     } catch (error) {
-       if (error.name === "AbortError") {
-         console.log("abort");
-         setLoading(false);
-       } else {
-         errorFun(error.message);
-         setLoading(false);
-       }
-     }
-   };  
+    const abortConst = new AbortController();
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url, { signal: abortConst.signal });
+        const data = await res.json();
+        setLoading(false);
+        setUsers(data.results);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("abort");
+          setLoading(false);
+        } else {
+          errorFun(error.message);
+          setLoading(false);
+        }
+      }
+    };
     fetchData();
 
-  return () => {
-   abortConst.abort();
-  };
+    return () => {
+      abortConst.abort();
+    };
   }, [url]);
+  const filterNat = (nat) => {
+    if (nat !== undefined) {
+      setUsers((prev) => prev.filter((user) => nat.includes(user.nat) ));
+      setUsers(prev=>prev.length==0?'':prev)
+     
+    }
+  };
 
-  return { loading, users,filterNat,filterGen };
+  const filterGen = (gen) => {
+    setUsers((prev) => prev.filter((user) => gen.includes(user.gender)));
+  
+  };
+
+  return { loading, users, filterNat, filterGen};
 }
