65bc1e337c95f111e9f1dc43fcd6c7630151d7f7
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-26 12:35:24 +0100

    Changed route

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,23 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import Redirect from "../components/Redirect";
+import Redirect from "../pages/Redirect";
 import Contact from "../pages/Contact";
 import Home from "../pages/Home";
-import Female from "../pages/Female";
-import Male from "../pages/Male";
+import Faq from "../pages/Faq";
+import Social from "../pages/Social";
 import User from "../pages/User";
 import Users from "../pages/Users";
+import Nouser from "../pages/Nouser";
 
 function ProjectRoute() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="users" element={<Users />}>
-          <Route path="female" element={<Female />} />
-          <Route path="male" element={<Male />} />
-        </Route>
+        <Route path="users" element={<Users />}></Route>
         <Route path="/user/:id" element={<User />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />}>
+          <Route path="fact" element={<Faq />} />
+          <Route path="dev" element={<Social />} />
+        </Route>
+        <Route path='nobuddy' element={<Nouser />} />
         <Route path="*" element={<Redirect />} />
       </Routes>
     </>
